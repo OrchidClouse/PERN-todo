65c3fade4362e6fc8fae866b9b6556b0e42fc4d5
OrchidClouse <maks67topcheg@yandex.ru>
2023-05-27 15:20:04 +0300

    fixed footer

--- react/src/Components/Footer/Footer.js
+++ react/src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-neutral-200 text-center text-white dark:bg-neutral-600 mt-10 absolute bottom-0 w-full h-10">
+    <footer className="bg-neutral-200 text-center text-white dark:bg-neutral-600 mt-10 fixed bottom-0 w-full h-10">
 
       <div
         className="bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
--- react/src/Routes/root.js
+++ react/src/Routes/root.js
@@ -5,14 +5,14 @@ import {Link} from "react-router-dom";
 
 export default function Root() {
   return (
-      <div className="flex min-h-full flex-col w-full items-center m-auto px-6 py-12 lg:px-8 ">
+      <div className="flex min-h-full flex-col w-full items-center m-auto px-6 py-12 lg:px-8 h-max">
         <h1 className="text-4xl">Hello!</h1>
         <br/>
         <p className="text">It's a Gira. Advanced ToDo for development.</p>
         <div className="pb-4">
           <Link className="hover:text-blue-500 hover:underline active:text-blue-500 w-max" to="/signup">For use u should register</Link>
         </div>
-        <Link className="bg-blue-600 w-48 text-white text-center border-2 rounded-full" to={`/login`}>Go to Login</Link>
+        <Link className="bg-blue-600 w-48 text-white text-center border-2 rounded-full" to="/login">Go to Login</Link>
       </div>
   );
 }
