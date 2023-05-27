6c23584540f270a78a930e350557088aebd3025f
OrchidClouse <maks67topcheg@yandex.ru>
2023-05-27 13:38:18 +0300

    create server in go

---
+++ golang/db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users
+( user_id int PRIMARY KEY,
+  username varchar(25) NOT NULL,
+  password varchar(30) NOT NULL
+);
---
+++ golang/server/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+var people []User
+
+func main() {
+	http.HandleFunc("/", mainHandler)
+	fmt.Println("server started on port 8080")
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getPeople(w, r)
+	case http.MethodPost:
+		postUser(w, r)
+	default:
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+
+	}
+}
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+	fmt.Fprintf(w, "get new person: '%v'", people)
+}
+
+func postUser(w http.ResponseWriter, r *http.Request) {
+	var person User
+	err := json.NewDecoder(r.Body).Decode(&person)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	people = append(people, person)
+	fmt.Println(people)
+	fmt.Fprintf(w, "post new person: '%v'", person)
+}
